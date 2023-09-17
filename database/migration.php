26a8504170a916d1dcf73eeb9b5c77e90925cabe
hollakz <hollaawp@mail.ru>
2023-09-17 12:33:58 +0300

    Создал файл database/migration.php, таблицу users с данными.

---
+++ database/migration.php
@@ -0,0 +1,22 @@
+<?php
+require ('migration.php');
+
+$db = new  SQLite3('database.sqlite');
+
+$sql = "CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+password STRING,
+level TEXT,
+integer INTEGER                     
+)";
+$db->exec($sql);
+
+$sql = "INSERT INTO users VALUES (1, 'paulchervov', 'pasSworLd102', 'C1', 'admin'),
+                         (2, 'hollakz', 'Coca-Holla42', 'А1', 'admin'),
+                         (3, 'olga', 'olga-0-conor-99', 'C2', 'admin');";
+$db->exec($sql);
+
+
+
+
