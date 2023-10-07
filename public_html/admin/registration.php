4f022d1a59c8874298bf3725107ea5461723d345
paulcervov <paulcervov@gmail.com>
2023-10-08 00:38:40 +0700

    Improved registration.php

--- public_html/admin/registration.php
+++ public_html/admin/registration.php
@@ -1,3 +1,37 @@
+<?php
+$pdo = new PDO('sqlite:/Users/pavelchervov/PhpstormProjects/masterlingua/database/database.sqlite');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$registrationMessage = '';
+$registrationError = false;
+
+if (($_SERVER['REQUEST_METHOD'] === 'POST') && isset($_POST["register"])) {
+
+    $username = mb_substr($_POST['username'] ?? '', 0, 20);
+    $password = mb_substr($_POST['password'] ?? '', 0, 10);
+
+    if (!empty($username) && !empty($password)) {
+
+        try {
+
+            $sqlInsert = "INSERT INTO users (username, password) VALUES (:username, :password)";
+            $stmt = $pdo->prepare($sqlInsert);
+            $stmt->bindValue(':username', strtolower($username));
+            $stmt->bindValue(':password', $password);
+            $stmt->execute();
+            $registrationMessage = 'Регистрация прошла успешно!';
+        } catch (PDOException $e) {
+
+            $registrationError = true;
+            if (str_contains($e->getMessage(), 'UNIQUE')) {
+                $registrationMessage = 'Пользователь с таким username уже существует, придумайте другой username.';
+            } else {
+                $registrationMessage = 'Ошибка регистрации, обратитесь в службу поддержки!';
+            }
+        }
+    }
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,42 +45,40 @@
 </head>
 <body>
 
-<div class="container p-5">
-    <h3 class="position-absolute top-0 start-50 translate-middle-x">Регистрация</h3>
-<form action="" class="offset-md-1 position-absolute top-30 start-50 translate-middle-x" method="post">
-    <div class="col-md-7 mb-3">
-        <label for="username" class="form-label">E-mail</label>
-        <input type="email" class="form-control" name="username"  id="username" aria-describedby="emailHelp" maxlength="20">
-        <div id="emailHelp" class="form-text">Мы никогда не передадим ваш адрес электронной почты кому-либо еще.</div>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6">
+
+            <div class="registration pt-3">
+                <h3 class="text-center">Регистрация</h3>
+
+                <?php if(!empty($registrationMessage)): ?>
+                    <div class="alert <?php if($registrationError): ?>alert-danger<?php else: ?>alert-success<?php endif; ?>" role="alert">
+                        <?php echo $registrationMessage; ?>
+                        <a href="/admin">В админку</a>
+                    </div>
+                <?php endif; ?>
+
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" name="username" id="username" aria-describedby="emailHelp" minlength="1" maxlength="20" required="required">
+                        <div id="emailHelp" class="form-text">Мы никогда не передадим ваш адрес электронной почты кому-либо еще.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Пароль</label>
+                        <input type="password" class="form-control" id="password" name="password" minlength="1" maxlength="10" required="required">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="register">Зарегистрироваться</button>
+                    <a class="btn btn-light inline-block" href="/admin/registration.php" role="button">Сброс</a>
+                </form>
+
+            </div>
+        </div>
     </div>
-    <div class="col-md-7 mb-3">
-        <label for="password" class="form-label">Пароль</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary" name="register">Зарегистрироваться</button>
-</form>
 </div>
-</body>
-</html>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    $pdo = new PDO('sqlite:C:\Users\holla\PhpstormProjects\masterlingua\database\database.sqlite');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sqlInsert = "INSERT INTO users (username, password) VALUES (:username, :password)";
-    $stmt = $pdo->prepare($sqlInsert);
-    $stmt->bindValue(':username', $username);
-    $stmt->bindValue(':password', $password);
-    $stmt->execute();
-
-    $pdo = null;
-    echo "Регистрация прошла успешно!";
-}
-?>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
         crossorigin="anonymous"></script>
+</body>
+</html>
