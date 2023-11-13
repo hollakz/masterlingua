6591660572ce134ec544e40f3c8f066eaffe9f06
hollakz <hollaawp@mail.ru>
2023-11-13 21:24:27 +0300

    Добавлено условие создающая новую таблицу с нуля.

--- database/migration.php
+++ database/migration.php
@@ -1,5 +1,15 @@
 <?php
 
+$arg = $argv[1] ?? null;
+
+if ($arg === '--fresh') {
+    try {
+        unlink('./database/database.sqlite');
+    } catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+}
+
 try {
     $pdo = new PDO('sqlite:./database/database.sqlite');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -16,9 +26,21 @@ try {
     )";
     $pdo->exec($sqlCreateTable);
 
-    $sqlInsertData = "INSERT INTO users (username, password, level, role) VALUES
-        ('admin', 'pasSworLd102', 'A1', 'admin')";
+    $sqlInsertData = "INSERT INTO users (username, password, level, role, first_name, last_name, date_of_birth) VALUES
+        ('admin', 'pasSworLd102', 'A1', 'admin', 'Alexey', 'Klimenkov', '28.05.1991'),
+        ('student1', 'qwe123', 'A1', 'student', 'Sergay', 'Petrov', '22.03.1994'),
+        ('teacher1', 'qwe123', 'C2', 'teacher', 'Pavel', 'Ivanov', '11.12.1990')";
     $pdo->exec($sqlInsertData);
+
+    //Создание таблицы Tasks
+    $sqlCreateTable = "CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        description TEXT,
+        teacher_id INTEGER,
+        student_id INTEGER
+            )";
+    $pdo->exec($sqlCreateTable);
 
     echo "Table created and data inserted successfully.";
 } catch (PDOException $e) {
