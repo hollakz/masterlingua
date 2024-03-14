a9ee1548b74f6ac936798f58e5d1919103448cdc
hollakz <hollaawp@mail.ru>
2024-03-14 22:15:11 +0300

    Переделал форму для новоого пароля

--- public_html/admin/reset_password.php
+++ public_html/admin/reset_password.php
@@ -31,11 +31,35 @@ if($result->rowCount() == 0) {
         echo "Пароль успешно изменен.";
     } else {
         ?>
-        <form method="post">
-            <label for="new_password">Введите новый пароль:</label>
-            <input type="password" id="new_password" name="new_password" required>
-            <button type="submit">Изменить пароль</button>
-        </form>
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+                  integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+            <title>Восстановление доступа</title>
+        </head>
+        <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-8 col-md-6 col-lg-4 pt-5">
+                    <form class="form-signin" method="post">
+                        <h2 class="form-signin-heading text-center">Восстановление доступа</h2>
+                        <label for="new_password" class="sr-only mb-2">Введите новый пароль:</label>
+                        <input type="password" id="new_password" name="new_password" required class="form-control" placeholder="New password" autofocus>
+                        <button class="btn btn-primary btn-block mt-3" type="submit">Изменить пароль</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+                crossorigin="anonymous"></script>
+        </body>
+        </html>
         <?php
     }
 }
